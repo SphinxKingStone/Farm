c9c4d7a5c691e8a78788b203e337bfd4cbf3eaa0
Maxim Mishakov <Sphinx_king_stone@mail.ru>
2018-05-03 11:28:06 +0300

    Убрал лишние указатели

--- Farm/functions_for_persons.cpp
+++ Farm/functions_for_persons.cpp
@@ -3,19 +3,9 @@
 
 bool avoid(int base_value, double min_value)
 {
-    double * chance = new double();
-    double * my_chance = new double();
-    *chance = static_cast<double>(qrand()) / RAND_MAX;
-    *chance = (min_value + (*chance) * (100.0 - min_value));
-    *my_chance = base_value * min_value;
+    double chance = static_cast<double>(qrand()) / RAND_MAX;
+    chance = (min_value + (*chance) * (100.0 - min_value));
+    double my_chance = base_value * min_value;
 
-    if (*my_chance >= *chance)
-    {
-        delete my_chance;
-        delete chance;
-        return true;
-    }
-    delete my_chance;
-    delete chance;
-    return false;
+    return my_chance >= chance;
 }
